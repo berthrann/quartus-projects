3871bcec957128cba2147ba0f5394eadab917188
berthrann <richardberthrann@gmail.com>
2021-02-05 17:41:53 +0300

    HelloWorld софтпроцессора NIOS2 (должен мигать светодиодами)

---
+++ NIOS_LED/nios_basic/nios_basic_bb.v
@@ -0,0 +1,10 @@
+
+module nios_basic (
+	clk_clk,
+	reset_reset_n,
+	leds_export);	
+
+	input		clk_clk;
+	input		reset_reset_n;
+	output	[3:0]	leds_export;
+endmodule
---
+++ NIOS_LED/software/hello_nios/obj/default/main.d
@@ -0,0 +1,14 @@
+obj/default/main.o: main.c ../hello_nios_bsp/system.h \
+ ../hello_nios_bsp/linker.h \
+ ../hello_nios_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../hello_nios_bsp//HAL/inc/io.h ../hello_nios_bsp//HAL/inc/alt_types.h
+
+../hello_nios_bsp/system.h:
+
+../hello_nios_bsp/linker.h:
+
+../hello_nios_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../hello_nios_bsp//HAL/inc/io.h:
+
+../hello_nios_bsp//HAL/inc/alt_types.h:
